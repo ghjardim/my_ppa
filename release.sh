49ebd5915925ad15be61e58ae1ccd26a194290f4
Guilherme H. Jardim <>
2023-12-30 23:53:11 -0300

    update

---
+++ release.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+[ "$EMAIL" ] || { echo "Provide EMAIL"; exit -1; }
+
+# Packages & Packages.gz
+dpkg-scanpackages --multiversion . > Packages
+gzip -k -f Packages
+
+# Release, Release.gpg & InRelease
+apt-ftparchive release . > Release
+gpg --default-key "${EMAIL}" -abs -o - Release > Release.gpg
+gpg --default-key "${EMAIL}" --clearsign -o - Release > InRelease
+
+# Commit & push
+git add -A
+git commit -m update
+git push
